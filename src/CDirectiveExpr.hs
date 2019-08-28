3d2dcd7a5c1af74f30888c1775b522677eaf3aff
andyshafer <andyshafer0715@gmail.com>
2019-08-28 14:14:49 -0400

    Added expression evaluator

--- src/CDirectiveExpr.hs
+++ src/CDirectiveExpr.hs
@@ -1,5 +1,6 @@
 module CDirectiveExpr where
 
+import Data.Bits
 import Text.Parsec
 import Text.Parsec.String
 import Text.Parsec.Expr
@@ -56,3 +57,37 @@ term = m_parens exprparser
 infixOp op duop = Infix (m_reservedOp op >> return (Duo duop)) AssocLeft
 
 prefixOp op unop = Prefix (m_reservedOp op >> return (Uno unop))
+
+evalExpr :: Expr -> Int
+evalExpr (Constant n) = n
+evalExpr (Uno Pos e) = evalExpr e
+evalExpr (Uno Neg e) = evalExpr e * (-1)
+evalExpr (Uno Not e) = case evalExpr e of
+                           0 -> 1
+                           _ -> 0
+evalExpr (Uno BitNot e) = complement $ evalExpr e
+evalExpr (Duo Times e1 e2) = eval_infix e1 e2 (*)
+evalExpr (Duo Divide e1 e2) = eval_infix e1 e2 quot
+evalExpr (Duo Mod e1 e2) = eval_infix e1 e2 mod
+evalExpr (Duo Plus e1 e2) = eval_infix e1 e2 (+)
+evalExpr (Duo Minus e1 e2) = eval_infix e1 e2 (-)
+evalExpr (Duo ShiftL e1 e2) = eval_infix e1 e2 shiftL
+evalExpr (Duo ShiftR e1 e2) = eval_infix e1 e2 shiftR
+evalExpr (Duo Less e1 e2) = eval_infix e1 e2 (\n1 n2 -> boolToInt $ n1 < n2)
+evalExpr (Duo Greater e1 e2) = eval_infix e1 e2 (\n1 n2 -> boolToInt $ n1 > n2)
+evalExpr (Duo LTEQ e1 e2) = eval_infix e1 e2 (\n1 n2 -> boolToInt $ n1 <= n2)
+evalExpr (Duo GTEQ e1 e2) = eval_infix e1 e2 (\n1 n2 -> boolToInt $ n1 >= n2)
+evalExpr (Duo Equal e1 e2) = eval_infix e1 e2 (\n1 n2 -> boolToInt $ n1 == n2)
+evalExpr (Duo NotEqual e1 e2) = eval_infix e1 e2 (\n1 n2 -> boolToInt $ n1 /= n2)
+evalExpr (Duo BitAnd e1 e2) = eval_infix e1 e2 (.&.)
+evalExpr (Duo BitXor e1 e2) = eval_infix e1 e2 xor
+evalExpr (Duo BitOr e1 e2) = eval_infix e1 e2 (.|.)
+evalExpr (Duo And e1 e2) = eval_infix e1 e2 (\n1 n2 -> if (n1 /= 0) && (n2 /= 0) then 1 else 0)
+evalExpr (Duo Or e1 e2) = eval_infix e1 e2 (\n1 n2 -> if (n1 /= 0) || (n2 /= 0) then 1 else 0)
+
+eval_infix :: Expr -> Expr -> (Int -> Int -> Int) -> Int
+eval_infix e1 e2 op = (evalExpr e1) `op` (evalExpr e2)
+
+boolToInt :: Bool -> Int
+boolToInt True = 1
+boolToInt False = 0
